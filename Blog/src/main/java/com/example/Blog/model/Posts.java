d7c7763dcaf2c16a47c35c835bd462dcacfefd70
hollowsssd <131234928+hollowsssd@users.noreply.github.com>
2025-07-29 15:13:32 +0700

    fix import

--- Blog/src/main/java/com/example/Blog/model/Posts.java
+++ Blog/src/main/java/com/example/Blog/model/Posts.java
@@ -34,9 +34,6 @@ public class Posts {
     @JoinColumn(name = "user_id", nullable = false)
     private Users user;
 
-    @Column(name = "title", nullable = false)
-    private String title;
-
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
