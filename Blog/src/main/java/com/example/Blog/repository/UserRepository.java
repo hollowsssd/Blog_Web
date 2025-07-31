06ebc1a85b7ad4ad6543f4e01bc8a87988146ab8
hollowsssd <131234928+hollowsssd@users.noreply.github.com>
2025-07-31 20:29:49 +0700

    fix resources

--- Blog/src/main/java/com/example/Blog/repository/UserRepository.java
+++ Blog/src/main/java/com/example/Blog/repository/UserRepository.java
@@ -3,19 +3,15 @@ package com.example.Blog.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.Blog.model.Users;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
-        // Optional<Users> findByEmail(String email);
         List<Users> findByNameContaining(String Keyword);
         boolean existsByEmail(String email); // ← Câu lệnh kiểm tra
         List<Users> findByBanned(boolean banned);
-
-
         Users findByEmail(String email);
 
 }
