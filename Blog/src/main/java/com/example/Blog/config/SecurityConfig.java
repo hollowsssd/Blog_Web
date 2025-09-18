46f42c9ac1922d65cb9151d760d5fd1ca8193116
hollowsssd <131234928+hollowsssd@users.noreply.github.com>
2025-09-18 23:51:12 +0700

    add environment and security config

--- Blog/src/main/java/com/example/Blog/config/SecurityConfig.java
+++ Blog/src/main/java/com/example/Blog/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://fruits-quizzes-colleagues-bowl.trycloudflare.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://treatment-between-alternative-las.trycloudflare.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
